8511a7dcf6998a8623b6e442cd53bb9c7e607a16
Brian Beltran <brianbeltran93@gmail.com>
2023-09-13 15:09:22 -0400

    Post Page added, Navbar edited
    - Post page added
    - Navbar edited with link to Post page

---
+++ Blog/Pages/Post.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Pages
+{
+    public class PostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
